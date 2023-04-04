1992cf6fa1a5bb48b14aff028dfe7ec842448a50
Harry Raj <113687752+harry10-git@users.noreply.github.com>
2023-04-04 16:01:44 +1000

    Update q3.sql

--- Lab 11/q3.sql
+++ Lab 11/q3.sql
@@ -7,7 +7,7 @@ create or replace trigger ques3
 before insert on instructor
 for each row
 begin
-   if :new.salary > 0 then
+   if :new.salary > 0 and :new.name like '[A-Za-z]%' then
     insert into tableCheck values ('correct');
     else 
          insert into tableCheck values ('wrong');
